c8528b5d865b6cff412a973fc73c7e0d692d3d97
Kareem <Kareem@DESKTOP-QVJ0L66>
2020-02-01 18:19:59 +0200

    new Commit

---
+++ src/hr/NewClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr;
+
+/**
+ *
+ * @author Kareem
+ */
+public class NewClass {
+
+    public NewClass() {
+    }
+    
+    
+}
